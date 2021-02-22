a23076aa85967385802461562b65e89e32ed6f85
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-23 05:21:09 +0600

    Create main.lua

---
+++ lua/main.lua
@@ -0,0 +1,11 @@
+
+-- Global helper function table.
+helper = wesnoth.require "lua/helper.lua"
+
+-- Load local code files.
+for i, file in ipairs {
+    'dialog/player_dialog.lua',
+}
+do
+    wesnoth.require('~add-ons/Knyght_MP_Campaign/lua/' .. file)
+end
