85978d6bbfc7d3ac30b3b62ea6522912371f8386
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-24 22:44:18 +0600

    Create end_campaign.lua

---
+++ lua/events/end_campaign.lua
@@ -0,0 +1,8 @@
+wesnoth.wml_actions.endlevel {
+    result = "victory",
+    linger_mode = true,
+    replay_save = false,
+    carryover_percentage = 0,
+    carryover_add = false,
+    carryover_report = false,
+}
