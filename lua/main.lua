1875ff8ef39acd6499f5e8e080996f09db25b2c3
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-25 02:04:11 +0600

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -5,6 +5,7 @@ helper = wesnoth.require "lua/helper.lua"
 -- Load local code files.
 for i, file in ipairs {
     'dialog/player_dialog.lua',
+    'scenarios/hero_deaths.lua',
 }
 do
     wesnoth.dofile('~add-ons/Knyght_MP_Campaign/lua/' .. file)
