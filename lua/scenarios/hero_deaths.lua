88c5adcaae56f4823f8233b14e9cc9727d5245c0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-25 04:16:37 +0600

    fixed broken code

--- lua/scenarios/hero_deaths.lua
+++ lua/scenarios/hero_deaths.lua
@@ -4,7 +4,7 @@ local _ = wesnoth.textdomain "wesnoth-kmpcamp"
 -- adding the event caller thingy?
 local on_event = wesnoth.require("on_event")
 
-on_event("die" function()
+on_event("die" function(cx)
     local u = wesnoth.units.get(cx.x1, cx.y1)
 	if (not u) or (not u:matches({ side = 3, canrecruit = true })) then
 		return
@@ -15,7 +15,7 @@ on_event("die" function()
 	end
 end)
 
-on_event("die" function()
+on_event("die" function(cx)
     local u = wesnoth.units.get(cx.x1, cx.y1)
 	if (not u) or (not u:matches({ side = 4, canrecruit = true })) then
 		return
