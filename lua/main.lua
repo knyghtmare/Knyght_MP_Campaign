3f69b5131d8861c1f081a84ea58d68ffea3f32d6
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-24 21:52:30 +0600

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -5,7 +5,6 @@ helper = wesnoth.require "lua/helper.lua"
 -- Load local code files.
 for i, file in ipairs {
     'dialog/player_dialog.lua',
-    'gameplay/recall_cost.lua',
 }
 do
     wesnoth.dofile('~add-ons/Knyght_MP_Campaign/lua/' .. file)
