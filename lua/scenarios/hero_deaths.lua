66fd54d8db70dae1d2b5258fbfee566927e984f0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-25 03:29:18 +0600

    Update hero_deaths.lua

---
+++ lua/scenarios/hero_deaths.lua
@@ -0,0 +1,27 @@
+-- stating the textdomain
+local _ = wesnoth.textdomain "wesnoth-kmpcamp"
+
+-- adding the event caller thingy?
+local on_event = wesnoth.require("on_event")
+
+on_event("die" function()
+    local u = wesnoth.units.get(cx.x1, cx.y1)
+	if (not u) or (not u:matches({ side = 3, canrecruit = true })) then
+		return
+    else
+        wesnoth.wml_actions.endlevel {
+            result = "defeat"
+        }
+	end
+end)
+
+on_event("die" function()
+    local u = wesnoth.units.get(cx.x1, cx.y1)
+	if (not u) or (not u:matches({ side = 4, canrecruit = true })) then
+		return
+    else
+        wesnoth.wml_actions.endlevel {
+            result = "defeat"
+        }
+	end
+end)
