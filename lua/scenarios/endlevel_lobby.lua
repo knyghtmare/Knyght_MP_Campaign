ec0e20bde242731b4656de9f9555359343fb8f1d
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-25 00:53:32 +0600

    Update endlevel_lobby.lua

--- lua/scenarios/endlevel_lobby.lua
+++ lua/scenarios/endlevel_lobby.lua
@@ -3,7 +3,7 @@ local next_level_var = wesnoth.get_variable "knyght_MP_next_random_scenario_lobb
 wesnoth.wml_actions.endlevel {
     result = "victory",
     next_scenario = next_level_var,
-    linger_mode = true,
+    linger_mode = false,
     replay_save = false,
     carryover_percentage = 0,
     carryover_add = false,
