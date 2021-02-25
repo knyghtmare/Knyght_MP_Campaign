8357484daf1ebb4028d05255e57412dde42d68cf
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-25 10:07:52 +0600

    Update recall_cost.lua

--- lua/gameplay/recall_cost.lua
+++ lua/gameplay/recall_cost.lua
@@ -5,6 +5,6 @@ for _, unit in ipairs(wesnoth.get_recall_units {}) do
     if unit.level == 0 then
         unit.recall_cost = cost0
     end
-    id unit.level == 1 then
+    if unit.level == 1 then
         unit.recall_cost = cost1
 end
