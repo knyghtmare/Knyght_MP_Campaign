ce2266f6556159515f8ec06c14b489e9a8d8a5c6
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-26 04:59:31 +0600

    Update drakes.lua

--- lua/enemy_types/drakes.lua
+++ lua/enemy_types/drakes.lua
@@ -1,5 +1,3 @@
--- local drake_leader = wesnoth.get_variable "leader_type"
-
 wesnoth.put_unit({
       side = 3,
       type = helper.rand("Drake Flameheart, Drake Bladermaster, Inferno Drake, Drake Warden, Drake Enforcer"),
@@ -12,9 +10,10 @@ wesnoth.put_unit({
 
 wesnoth.wml_actions.set_recruits {
     side = 3,
-    recruit = "Drake Burner, Drake Fighter, Drake Flare, Drake Clasher, Drake Arbiter, Drake Thrasher",
+    recruit = "Drake Burner, Drake Fighter, Saurian Augur, Saurian Skirmisher, Drake Flare, Drake Clasher, Drake Arbiter, Drake Thrasher",
   }
 
+-- Are the recalls too much?
 wesnoth.put_recall_unit({ type = helper.rand("Drake Blademaster, Drake Flameheart, Inferno Drake") }, 3)
 wesnoth.put_recall_unit({ type = helper.rand("Drake Blademaster, Drake Flameheart, Inferno Drake") }, 3)
 wesnoth.put_recall_unit({ type = helper.rand("Drake Blademaster, Drake Flameheart, Inferno Drake") }, 3)
