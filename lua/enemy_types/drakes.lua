a63054c4f37bf8471967d2bb409d3a3b59824b46
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-26 07:19:20 +0600

    Update drakes.lua

--- lua/enemy_types/drakes.lua
+++ lua/enemy_types/drakes.lua
@@ -1,7 +1,7 @@
 function knyght_mp_Campaign_enemy_side_setup(side)
   wesnoth.put_unit({
         side = side.side_num,
-        type = helper.rand("Drake Flameheart, Drake Bladermaster, Inferno Drake, Drake Warden, Drake Enforcer"),
+        type = helper.rand("Drake Flameheart","Inferno Drake","Drake Enforcer","Drake Warden","Drake Blademaster"),
         canrecruit = true,
         placement = leader,
         passable = true,
@@ -11,13 +11,13 @@ function knyght_mp_Campaign_enemy_side_setup(side)
 
   wesnoth.wml_actions.set_recruits {
       side = 3,
-      recruit = "Drake Burner, Drake Fighter, Saurian Augur, Saurian Skirmisher, Drake Flare, Drake Clasher, Drake Arbiter, Drake Thrasher",
+      recruit = "Drake Burner,Drake Clasher,Drake Glider,Drake Fighter,Saurian Skirmisher,Saurian Augur",
     }
 
   -- Are the recalls too much?
-  wesnoth.put_recall_unit({ type = helper.d("Drake Blademaster, Drake Flameheart, Inferno Drake") }, 3)
-  wesnoth.put_recall_unit({ type = helper.rand("Drake Blademaster, Drake Flameheart, Inferno Drake") }, 3)
-  wesnoth.put_recall_unit({ type = helper.rand("Drake Blademaster, Drake Flameheart, Inferno Drake") }, 3)
+  wesnoth.put_recall_unit({ type = helper.rand("Drake Warrior", "Drake Flare", "Sky Drake", "Saurian Ambusher", "Fire Drake", "Drake Thrasher", "Drake Arbiter", "Saurian Oracle", "Saurian Soothsayer") }, 3)
+  wesnoth.put_recall_unit({ type = helper.rand("Drake Warrior", "Drake Flare", "Sky Drake", "Saurian Ambusher", "Fire Drake", "Drake Thrasher", "Drake Arbiter", "Saurian Oracle", "Saurian Soothsayer") }, 3)
+  wesnoth.put_recall_unit({ type = helper.rand("Drake Warrior", "Drake Flare", "Sky Drake", "Saurian Ambusher", "Fire Drake", "Drake Thrasher", "Drake Arbiter", "Saurian Oracle", "Saurian Soothsayer") }, 3)
 end
 
 
